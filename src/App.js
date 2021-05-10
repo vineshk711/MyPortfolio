4cc3d67e0084696cf5a61e72040c4cabc983072f
Vinesh Kumar <63194027+vineshk711@users.noreply.github.com>
2021-05-10 14:34:00 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import { React } from "react";
+import "./styles.css";
+import Navbar from "./components/navbar";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Navbar />
+    </div>
+  );
+}
