4aab20ef12fc80e39b05a1acca80b9ba369be247
Vinesh Kumar <Vineshk711@gmail.com>
2021-05-15 03:49:00 +0000

    update

---
+++ src/components/navbar.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Navbar = () => (
+    <div>
+        <ul className="nav nav-tabs">
+            <li className="nav-item btn-primary">About Me</li>
+        </ul>
+    </div>
+)
+export default Navbar
