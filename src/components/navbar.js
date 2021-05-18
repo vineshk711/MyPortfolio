b692eed3ed68c2d01af3c5b960b6911a0bd28127
Vinesh Kumar <Vineshk711@gmail.com>
2021-05-18 12:19:40 +0000

    update

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -2,6 +2,7 @@ import React from 'react'
 import './componentStyles/navbar.css'
 const Navbar = () => (
     <div className="nav-container">
+        <h1 className="nav title">My Portfolio+</h1>
         <ul className="nav nav-tabs">
             <li className="nav-item btn-secondry">About Me</li>
             <li className="nav-item btn-secondry">Projects</li>
--- src/components/welcomeInfo.js
+++ src/components/welcomeInfo.js
@@ -4,8 +4,12 @@ import './componentStyles/welcomeInfo.css'
 export default function WelcomeInfo() {
     return (
         <div className="welcome-info-container">
-            <div className="hello-container">Hello</div>
-            
+            <div className="hello-container">
+                <div className="introduction">INTRO</div>
+                <div className="hello">Hello.</div>
+                <div className="detail">Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde similique voluptatibus placeat hic nam? Perferendis illum eligendi repellat in temporibus.</div>
+            </div>
+
         </div>
     )
 }
