c586eb4051c3d28074000ed40de0b01e100690d0
Vinesh Kumar <Vineshk711@gmail.com>
2021-05-18 06:14:47 +0000

    update

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -3,9 +3,9 @@ import './componentStyles/navbar.css'
 const Navbar = () => (
     <div className="nav-container">
         <ul className="nav nav-tabs">
-            <li className="nav-item btn-primary">About Me</li>
-            <li className="nav-item btn-primary">Projects</li>
-            <li className="nav-item btn-primary">Resume</li>
+            <li className="nav-item btn-secondry">About Me</li>
+            <li className="nav-item btn-secondry">Projects</li>
+            <li className="nav-item btn-secondry">Resume</li>
             <li className="nav-item btn-primary">Contact Info</li>
         </ul>
     </div>
