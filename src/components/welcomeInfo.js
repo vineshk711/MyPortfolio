f34f6867214bba2d8bb78a9ace066af4b5aab2cd
Vinesh Kumar <Vineshk711@gmail.com>
2021-05-19 06:03:36 +0000

    upate

--- src/components/welcomeInfo.js
+++ src/components/welcomeInfo.js
@@ -8,6 +8,10 @@ export default function WelcomeInfo() {
                 <div className="introduction">INTRO</div>
                 <div className="hello">Hello.</div>
                 <div className="detail">Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde similique voluptatibus placeat hic nam? Perferendis illum eligendi repellat in temporibus.</div>
+                <div className="btn-container">
+                    <div className="btn btn-primary">Resume</div>
+                    <div className="btn btn-primary btn-black">Projects</div>
+                </div>
             </div>
 
         </div>
