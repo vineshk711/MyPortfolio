b3a90d55955cffeb7ea7a05464391b8c7ba9add3
Vinesh Kumar <Vineshk711@gmail.com>
2021-06-06 05:11:58 +0000

    adding project 2

--- src/components/projectsPage.js
+++ src/components/projectsPage.js
@@ -1,15 +1,27 @@
 import React from 'react'
 import Card from './card'
-import photo from '../data/project1.jpeg'
+import project1Photo from '../data/project1.jpeg'
+import project2Photo from '../data/project2.jpeg'
+import './componentStyles/projectPage.css'
+
 export default function ProjectsPage() {
     return (
         <div>
-            <Card
-                photo={photo}
-                subtitle="E-COMMERCE WEBSITE WITH PAYMENT GATEWAY"
-                title="Sell Your Products"
-                description="The wuicsk borown fox jumpsj ovjak ihdk thel whye are ouf"
-            />
+            <div className="cards-container">
+                <Card className="project1"
+                    photo={project1Photo}
+                    subtitle="E-COMMERCE WEBSITE WITH PAYMENT GATEWAY"
+                    title="Sell Your Products"
+                    description="The wuicsk borown fox jumpsj ovjak ihdk thel whye are ouf"
+                />
+                <Card className="project2"
+                    photo={project2Photo}
+                    subtitle="A FRONTEND ONLY PROJECT"
+                    title="My Portfolio"
+                    description="This is my portfolio only forntend hosted on github.io"
+                />
+            </div>
+            
         </div>
     )
 }
